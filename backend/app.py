2f371ff2fde5921d6c4e80badaaaaa338e081061
Abigail Lin <abigail.lin@login7.ufhpc>
2026-02-13 19:40:08 -0500

    Implemented integrated gradients engine & added to app.py

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,49 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from attention_engine import get_attention_matrix
+from integrated_gradients_engine import IntegratedGradientsEngine
 
 app = Flask(__name__)
 CORS(app) # Allows the React frontend to talk to this Python server
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+#Load the ESM-2 model and alphabet once, to ominimize latency
+model_version = "esm2_t33_650M_UR50D"
+model, alphabet = esm.pretrained.load_model_and_alphabet(model_version)
+model = model.to(device)
+model.eval()
 
 @app.route('/get-attention', methods=['POST'])
 def handle_attention():
     # 1. Get the protein sequence sent from the React frontend
     data = request.json
     sequence = data.get("sequence")
-    
+
     if not sequence:
         return jsonify({"error": "No sequence provided"}), 400
 
     # 2. Call your verified extraction engine to get the real Layer 33 weights
     matrix = get_attention_matrix(sequence)
-    
+
     # 3. Send the matrix back to React to update the HeatmapGrid
     return jsonify({"matrix": matrix})
+
+ig_engine = IntegratedGradientsEngine(model, alphabet)
+
+@app.route('/get-integrated-gradients', methods=['POST'])
+def handle_integrated_gradients():
+    data = request.json
+    sequence = data.get("sequence")
+    position = data.get("position")
+    mutant = data.get("mutant")
+
+    if not sequence:
+        return jsonify({"error": "No sequence provided"}), 400
+
+    importance = ig_engine.compute(sequence, position, mutant)
+    return jsonify({"importance": importance.tolist()})
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
---
+++ backend/integrated_gradients_engine.py
@@ -0,0 +1,87 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+
+
+#Main engine behind my IG implementation
+class IntegratedGradientsEngine:
+    def __init__(self, model, alphabet, steps=50): 
+        self.model = model #ESM (650M?)
+        self.alphabet = alphabet #AA vocab i.e. token mappings
+        self.steps = steps #steps = "interpolation" steps between x -> x'
+        self.model.eval() #Set model to eval mode, disable layer dropout, makes output deterministic
+        self.device = next(model.parameters()).device #CPU/GPU set
+
+    #Computing the integrated gradients for one mutation
+    def compute(self, sequence, position, mutant_aa):
+
+        batch_converter = self.alphabet.get_batch_converter()
+        aa_to_idx = self.alphabet.tok_to_idx
+
+        #Convert raw AA string sequence to tokens
+        data = [("protein", sequence)]
+        _, _, tokens = batch_converter(data)
+        tokens = tokens.to(self.device)
+
+        #Get wild-type amino acid at that position
+        wt_aa = sequence[position] #0-indexed pos from string seq
+        wt_idx = aa_to_idx[wt_aa] #W-t integer token num ID
+        mut_idx = aa_to_idx[mutant_aa] #Mut integer token num ID
+
+        #Adjust pos for CSL token @ 0 index
+        token_position = position + 1
+
+        #Get embeddings as 3-d vector (1, seq_lin, embed_dim)
+        embeddings = self.model.embed_tokens(tokens)
+
+        #Baseline - zero vector
+        baseline = torch.zeros_like(embeddings)
+
+        #Compute the integrated gradients using the func
+        integrated_grads = self._integrated_gradient_func(embeddings, baseline, token_position, wt_idx, mut_idx)
+
+        residue_importance = integrated_grads.sum(dim=-1).squeeze(0) #Squeeze to remove 1st batch dim
+
+        return residue_importance.detach().cpu().numpy()
+    
+    def integrated_gradient_func(self, embeddings, baseline, token_position, wt_idx, mut_idx):
+
+        
+        scaled_embeddings = [
+            baseline + (float(i) / self.steps) * (embeddings - baseline) #From the paper, this is the (x’ _ alpha x (x - x’)) part
+            for i in range(self.steps + 1) #For steps = 50, this creates 51 scaled embeddings from baseline to input embedding
+        ]
+
+        grads = [] #List to store gradients from each scaled embedding, (batch, seq_len, embed_dim)
+
+        ##Compute gradient at each step
+        for scaled in scaled_embeddings:
+            scaled = scaled.clone().detach().requires_grad_(True) #Clone and set requires_grad to True to compute gradients
+
+            #We need gradients in embeddding space, i.e. not discrete, so we pass the scaled embeddings directly to the model instead of token IDs
+            #Bec tokens themselves are category labels, embeddings = data pts in R^d i.e. embedding dim space
+            #Need numerical vectors of model weights, not mere labels that only represent semantically what token "is" as humans interpret it e.g. A = alanine/1
+            outputs = self.model(inputs_embeds=scaled)
+            logits = outputs["logits"]
+
+            log_probs = F.log_softmax(logits, dim=-1) #Convert raw logits (vector) to log prob (same length vect), normalize them across 20 AA
+
+            mut_lp = log_probs[:, token_position, mut_idx] #Get log prob of mut AA at that position
+            wt_lp = log_probs[:, token_position, wt_idx] #Get log prob of wt AA at that position
+
+            mutation_score = (mut_lp - wt_lp).sum() #Log ratio of mut vs wt, sum over batch dim (if using >1 batxh size) bec NEED scalar
+
+            self.model.zero_grad() #Reset prev computed grads from prev steps
+            mutation_score.backward() #Get grad, PARTIAL derivative of dF/df_xi like in paper
+
+            grads.append(scaled.grad.detach()) #Change shape to (steps+1, batch, seq_len, embed_dim)
+
+        grads = torch.stack(grads)
+
+        #Reimann sum time
+        grads = (grads[:-1] + grads[1:]) / 2.0
+        avg_grads = grads.mean(dim=0) #Average over alpha dim, now (batch, seq_len, embed_dim)
+
+        integrated_grads = (embeddings - baseline) * avg_grads #Entire IG math formula
+
+        return integrated_grads #(batch, seq_len, embed_dim)
